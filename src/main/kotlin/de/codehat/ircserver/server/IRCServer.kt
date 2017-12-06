45618a95597326477fdc9035e73d86c05146f3cb
CodeHat <dev@codehat.de>
2017-12-06 09:14:06 +0100

    Implement IServer in IRCServer, Add IDEA files to .gitignore

--- src/main/kotlin/de/codehat/ircserver/server/IRCServer.kt
+++ src/main/kotlin/de/codehat/ircserver/server/IRCServer.kt
@@ -1,15 +1,26 @@
 package de.codehat.ircserver.server
 
 class IRCServer(private val host: String = "localhost",
-                private val port: Int = 6667) {
+                private val port: Int = 6667) : IServer {
 
     private var serverThread: ServerThread? = null
 
-    fun start(): Boolean {
+    override fun start(): Boolean {
         if (this.serverThread == null) {
             this.serverThread = ServerThread(this)
             this.serverThread!!.start()
         }
     }
+
+    override fun stop(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun isRunning(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+
 
 }
